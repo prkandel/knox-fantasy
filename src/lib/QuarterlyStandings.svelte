1ed5e2a68ae0ba4baf0661149d851905b3897c9d
prkandel <prkandel19@gmail.com>
2023-12-22 14:43:02 -0500

    Add 30 dollar info

--- src/lib/QuarterlyStandings.svelte
+++ src/lib/QuarterlyStandings.svelte
@@ -6,9 +6,9 @@
 
     const quarterHeadlineKey = {
         1: ["Q1 (GW1 to GW9) Points Leader ($30)", "q1TotalPoints"],
-        2: ["Q2 (GW10 to GW19) Points Leader", "q2TotalPoints"],
-        3: ["Q3 (GW20 to GW28) Points Leader", "q3TotalPoints"],
-        4: ["Q4 (GW29 to GW36) Points Leader", "q4TotalPoints"],
+        2: ["Q2 (GW10 to GW19) Points Leader ($30)", "q2TotalPoints"],
+        3: ["Q3 (GW20 to GW28) Points Leader ($30)", "q3TotalPoints"],
+        4: ["Q4 (GW29 to GW36) Points Leader ($30)", "q4TotalPoints"],
     }
 
     players.sort((a, b) => b[quarterHeadlineKey[quarter][1]] - a[quarterHeadlineKey[quarter][1]])
